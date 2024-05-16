e98745f31db73e5010b5b1fa74dd8220c43f44d8
letmly <102484956+letmly@users.noreply.github.com>
2024-05-16 22:24:44 +0300

    1

--- ML_labs/course_/wtf.py
+++ ML_labs/course_/wtf.py
@@ -1,26 +1,43 @@
+import numpy as np
 import matplotlib.pyplot as plt
-import cv2
+from PIL import Image
+import os
 
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
 
-# Отображение гистограммы
-axes[0].set_title(f'Комбинированная гистограмма')
-axes[0].set_xlabel('Яркость')
-axes[0].set_ylabel('Частота')
-images_names_of_type_ = ['red.png', 'red.png', 'red.png', 'red.png', 'red.png']
-images_of_type_ = [cv2.imread(f'./generated-or-not/chek_images/{img_name}') for img_name in images_names_of_type_]
+def load_images(folder_path):
+    images = []
+    for filename in os.listdir(folder_path):
+        img = Image.open(os.path.join(folder_path, filename))
+        images.append(img)
+    return images
 
-# Построение общей гистограммы для каждого канала цвета (красный, зелёный, синий)
-colors = ('r', 'g', 'b')
-for i, col in enumerate(colors):
-    col_vals = [image[:, :, i].flatten() for image in images_of_type_]
-    hist = cv2.calcHist(col_vals, [0], None, [256], [0, 256])
-    axes[1].plot(hist, color=col, label=col)
-    axes[1].set_xlim([0, 256])
 
-axes[0].set_title(f'Поканальная гистограмма изображений')
-axes[1].set_xlabel('Яркость')
-axes[1].set_ylabel('Частота')
+def calculate_color_distribution(images):
+    colors = []
+    for img in images:
+        img_data = np.array(img)
+        colors.extend(img_data.reshape(-1, img_data.shape[2]))
 
-plt.tight_layout()
-plt.show()
+    return np.array(colors)
+
+
+def plot_color_histogram(color_distribution):
+    plt.figure(figsize=(10, 6))
+    plt.hist(color_distribution, bins=256, color=['red', 'green', 'blue'], alpha=0.7)
+    plt.xlabel('Intensity')
+    plt.ylabel('Frequency')
+    plt.title('Color Distribution')
+    plt.show()
+
+
+# Путь к вашему датасету с изображениями
+dataset_folder = "./generated-or-not/chek_images/"
+
+# Загрузка изображений
+images = load_images(dataset_folder)
+
+# Расчет распределения цветов
+color_distribution = calculate_color_distribution(images)
+
+# Построение гистограммы
+plot_color_histogram(color_distribution)
